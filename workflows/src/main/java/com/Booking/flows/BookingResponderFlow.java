b45f202600167bd6fb8e2364ade1fae7032ddc2e
dhinoja15 <60249765+dhinoja15@users.noreply.github.com>
2020-04-03 10:46:04 -0400

    updated

--- workflows/src/main/java/com/Booking/flows/BookingResponderFlow.java
+++ workflows/src/main/java/com/Booking/flows/BookingResponderFlow.java
@@ -17,37 +17,16 @@ import static net.corda.core.contracts.ContractsDSL.requireThat;
 public class BookingResponderFlow extends FlowLogic<SignedTransaction> {
     private FlowSession counterpartySession;
 
-    public BookingResponderFlow(FlowSession counterpartySession) {this.counterpartySession = counterpartySession; }
+    public BookingResponderFlow(FlowSession counterpartySession) {
+        this.counterpartySession = counterpartySession;
+    }
 
     @Suspendable
     @Override
     public SignedTransaction call() throws FlowException {
         // Responder flow logic goes here.
-        class SignTxFlow extends SignTransactionFlow {
-            private SignTxFlow(FlowSession otherPartyFlow, ProgressTracker progressTracker) {
-                super(otherPartyFlow, progressTracker);
-            }
-            @Override
-            protected void checkTransaction(SignedTransaction stx) {
-                requireThat(require -> {
-                    ContractState output = stx.getTx().getOutputs().get(0).getData();
-                    BookingState bookStateObj = (BookingState) output;
-                    require.using("This must be an Booking transaction.", output instanceof BookingState);
-                    require.using("Credit Card not Valid Ha Ha ", bookStateObj.getCreditCardNumber().length() == 16);
-                    String lastFourDigitCC = bookStateObj.getCreditCardNumber().substring(13,16);
-                    System.out.println("Reservation is confirmed : " + counterpartySession.getCounterparty().getName().getOrganisation());
-                    System.out.println("Credit Card Ends with "+ lastFourDigitCC + " is charged for Amount: "+bookStateObj.getCreditCardAmount());
-                    return null;
-                });
-            }
-        }
-
-        //final SignTxFlow signTxFlow = new SignTxFlow(counterpartySession, SignTransactionFlow.Companion.tracker());
-        //final SecureHash txId = subFlow(signTxFlow).getId();
-
         System.out.println("Booking Request received from : " + counterpartySession.getCounterparty().getName().getOrganisation());
 
-        //return subFlow(new ReceiveFinalityFlow(counterpartySession));
-        return subFlow(new SignTxFlow(counterpartySession, SignTransactionFlow.Companion.tracker()));
+        return subFlow(new ReceiveFinalityFlow(counterpartySession));
     }
 }
