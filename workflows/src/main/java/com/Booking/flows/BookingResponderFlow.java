ab907a1d1d31bbfdcf0f2a8b2c2ae2b9b4bdda99
dhinoja15 <60249765+dhinoja15@users.noreply.github.com>
2020-04-15 14:18:43 -0400

    Update BookingResponderFlow.java

--- workflows/src/main/java/com/Booking/flows/BookingResponderFlow.java
+++ workflows/src/main/java/com/Booking/flows/BookingResponderFlow.java
@@ -1,5 +1,4 @@
 package com.Booking.flows;
-
 import co.paralleluniverse.fibers.Suspendable;
 import com.Booking.states.BookingState;
 import net.corda.core.contracts.ContractState;
@@ -7,26 +6,21 @@ import net.corda.core.crypto.SecureHash;
 import net.corda.core.flows.*;
 import net.corda.core.transactions.SignedTransaction;
 import net.corda.core.utilities.ProgressTracker;
-
 import static net.corda.core.contracts.ContractsDSL.requireThat;
-
 // ******************
 // * Responder flow *
 // ******************
 @InitiatedBy(BookingInitiatorFlow.class)
 public class BookingResponderFlow extends FlowLogic<SignedTransaction> {
     private FlowSession counterpartySession;
-
     public BookingResponderFlow(FlowSession counterpartySession) {
         this.counterpartySession = counterpartySession;
     }
-
     @Suspendable
     @Override
     public SignedTransaction call() throws FlowException {
         // Responder flow logic goes here.
         System.out.println("Booking Request received from : " + counterpartySession.getCounterparty().getName().getOrganisation());
-
         return subFlow(new ReceiveFinalityFlow(counterpartySession));
     }
 }
