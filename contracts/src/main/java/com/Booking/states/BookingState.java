d1f7da982562eeefd13d43104f68b0737a272125
dhinoja15 <60249765+dhinoja15@users.noreply.github.com>
2020-04-16 13:41:41 -0400

    Update BookingState.java

--- contracts/src/main/java/com/Booking/states/BookingState.java
+++ contracts/src/main/java/com/Booking/states/BookingState.java
@@ -1,7 +1,6 @@
 package com.Booking.states;
-
 import com.Booking.contracts.BookingContract;
-//import io.netty.util.concurrent.AbstractFuture;
+import io.netty.util.concurrent.AbstractFuture;
 import net.corda.core.contracts.BelongsToContract;
 import net.corda.core.contracts.ContractState;
 import net.corda.core.identity.AbstractParty;
@@ -9,7 +8,6 @@ import net.corda.core.identity.Party;
 import java.time.Instant;
 import java.util.Arrays;
 import java.util.List;
-
 // *********
 // * State *
 // *********
@@ -26,7 +24,6 @@ public class BookingState implements ContractState {
     private final double creditCardAmount;
     private final Party BookYourStay;
     private final Party HotelHeaven;
-
     public BookingState(String custName, int custAge, Instant checkInDate, Instant checkOutDate, String roomType, int roomRate, String creditCardNumber, Instant creditCardExpDate, double creditCardAmount, Party bookYourStay, Party hotelHeaven) {
         this.custName = custName;
         this.custAge = custAge;
@@ -40,51 +37,39 @@ public class BookingState implements ContractState {
         this.BookYourStay = bookYourStay;
         this. HotelHeaven = hotelHeaven;
     }
-
     public String getCustName() {
         return custName;
     }
-
     public int getCustAge() {
         return custAge;
     }
-
     public Instant getCheckInDate() {
         return checkInDate;
     }
-
     public Instant getCheckOutDate() {
         return checkOutDate;
     }
-
     public String getRoomType() {
         return roomType;
     }
-
     public int getRoomRate() {
         return roomRate;
     }
-
     public String getCreditCardNumber() {
         return creditCardNumber;
     }
-
     public Instant getCreditCardExpDate() {
         return creditCardExpDate;
     }
-
     public double getCreditCardAmount() {
         return creditCardAmount;
     }
-
     public Party getBookYourStay() {
         return BookYourStay;
     }
-
     public Party getHotelHeaven() {
         return HotelHeaven;
     }
-
     @Override
     public List<AbstractParty> getParticipants() {
         return Arrays.asList(BookYourStay,HotelHeaven);
